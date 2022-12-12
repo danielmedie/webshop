c70f45205a638bb92168cb6922dd1416b5936852
henrietta jeansson <henriettajeansson@gmail.com>
2022-12-12 13:02:17 +0100

    start of basket buttons

---
+++ src/ts/models/basket.ts
@@ -0,0 +1,5 @@
+let theCart = [];
+
+let addCartItemButton = document.getElementsByClassName ("plusItem");
+let deleteCartItemButton = document.getElementsByClassName ("deleteItem");
+let checkoutCartItemButton = document.getElementsByClassName ("checkoutCart");
