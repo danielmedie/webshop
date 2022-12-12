5c922d770ea56028fcb33068ee6d776bb51955e2
henrietta jeansson <henriettajeansson@gmail.com>
2022-12-12 13:54:21 +0100

    buttons for cart

--- src/ts/models/basket.ts
+++ src/ts/models/basket.ts
@@ -1,5 +1,16 @@
 let theCart = [];
 
-let addCartItemButton = document.getElementsByClassName ("plusItem");
-let deleteCartItemButton = document.getElementsByClassName ("deleteItem");
-let checkoutCartItemButton = document.getElementsByClassName ("checkoutCart");
+let addCartItemButtons: HTMLButtonElement = document.getElementsByClassName ("addOneItem") as unknown as HTMLButtonElement;
+//addCartItemButton.innerHTML = "+";
+let deleteCartItemButtons: HTMLButtonElement = document.getElementsByClassName ("subOneItem") as unknown as HTMLButtonElement;
+//deleteCartItemButton.innerHTML = "-";
+let checkoutCartItemButtons: HTMLButtonElement = document.getElementsByClassName("deleteAllItems") as unknown as HTMLButtonElement;
+
+//checkoutCartItemButton.innerHTML = "Gå till kassan";
+
+// for (let i = 0; i < deleteCartItemButton.length; i ++ ){
+//         let button : any = deleteCartItemButton [i];
+//     button.addEventListener("click", function(event){
+//         event.target
+//     })
+// }
